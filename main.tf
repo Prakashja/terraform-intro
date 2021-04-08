4b721d7f2cf2b2d06f8f8017a91a72e7a970d8c4
Prakashja <jayaprakash0394@gmail.com>
2021-04-08 15:44:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,18 @@ provider "aws" {
 }
 
 
+resource "aws_instance" "myinstance" {
+  ami             = var.instance_ami
+  instance_type   = var.instance_type
+  subnet_id       = var.subnet
+  security_groups = [var.security_group]
+  key_name        = var.instance_key
+
+  tags = {
+    Name      = "myec2-instances"
+    Builder   = "terraform"
+    App       = "myapp"
+    owner     = "jayaprakahs.k"
+    contact   = "jai@gmail.com"
+  }
+}

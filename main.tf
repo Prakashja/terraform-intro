af6d19993a8a7ebaf52948ac8e7e9b7a9d5ebd30
Prakashja <jayaprakash0394@gmail.com>
2021-04-09 09:05:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region     = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
 }
-
 
 resource "aws_instance" "myinstance" {
   ami             = var.instance_ami
